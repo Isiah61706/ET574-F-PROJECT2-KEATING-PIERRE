ac07caf6153faa0de829abddea68ca3fc711a121
Isiah61706 <isiah617@icloud.com>
2025-12-04 20:39:29 +0000

    Bar graph customize start

--- Main.py
+++ Main.py
@@ -28,5 +28,28 @@ class BankWindowApp(wx.Frame):
         self,
         "Open Bank CSV", 
         wildcard="CSV files" (*.csv)|*.csv,
-        style = wx
-    )
+        style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST,
+        ) as dlg:
+            if dlg.ShowModal() != wx.ID_OK:
+                return
+            path = dlg.GetPath()
+
+        try:
+            df = pd.read_csv(path)
+            want = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+
+             # Normalize headers if present
+            df.columns = [str(c).strip().lower().replace(" ", "_") for c in df.columns]
+            if df.shape[1] == 5 and not set(want).issubset(df.columns):
+                # assume no headers; assign expected names
+                df.columns = want
+
+            # Basic checks
+            for c in ["sepal_length", "petal_length", "species"]:
+            #for c in ["sepal_width", "petal_width", "species"]:
+                if c not in df.columns:
+                    raise ValueError("CSV must contain 4 features + 'species'.")
+
+            plt.figure(figsize=(7.5, 5.0))
+            for sp, sub in df.groupby("species"):
+                plt.bar(sub[FEATURE_X]. sub[FEATURE_Y])
